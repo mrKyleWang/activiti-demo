cd3929d019252134cd55712496b0aba0aa5941cc
KyleWang <Kyle.Wang.cn@outlook.com>
2018-02-21 22:37:04 +0800

    启动类

--- src/main/java/top/kylewang/ActivitiApplication.java
+++ src/main/java/top/kylewang/ActivitiApplication.java
@@ -1,14 +1,15 @@
 package top.kylewang;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Kyle.Wang
  * 2018/2/20 0020 11:10
  */
-
-public class ActivitiApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class ActivitiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
     }
 }
